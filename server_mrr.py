59ecc4bd93655078ccf1ff638bb53404fe529a9b
mwangi victor <tmvicbvn@gmail.com>
2020-09-08 12:21:41 +0300

    Update server_mrr.py

--- server_mrr.py
+++ server_mrr.py
@@ -1,12 +1,15 @@
-'''
-imports
-'''
+import json
+import socket
+
+with open('cfgs.json', 'r') as reader:
+    cfgs = return reader.read()
+config = json.loads(cfgs)
 
 # incoming socket
 def __init__(self, config):
     signal.signal(signal.SIGINT, self.shutdown) # ctrl + c
     self.serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # reuse the socket
-    self.serverSocket.bind(config['HOST_NAME'], config['BIND_PORT']) # bind socket to host and port
+    self.serverSocket.bind(config['host'], config['bind_port']) # bind socket to host and port
     self.serverSocket.listen(10) # become server socket
     self.__clients = {}
     while True:
@@ -14,8 +17,9 @@ def __init__(self, config):
         d = threading.Thread(name = self.__getClientName(client_address), target = self.proxy_thread, args = (clientSocket, client_address))
         d.setDaemon(True)
         d.start()
-# 3 redirecting traffic
-request = conn.recv(config['max req len']) # get request
+        
+# redirecting traffic
+request = conn.recv(config['max_req_len']) # get request
 fline = request.split('\n')[0] # parse 1st line
 url = fline.split(' ')[1] # get url
 httppos = url.find('://')# pos of ://
@@ -35,14 +39,16 @@ if (portpos==-1 or webserverpos<portpos):
 else: # specific port
     port =int((temp[(portpos+1):])[:webserverpos-portpos-1])
     webserver=temp[:portpos]
+    
 #
 s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.settimeout(config['conn time out'])
+s.settimeout(config['time_out'])
 s.connect((webserver, port))
 s.sendall(request)
+
 # receive
 while 1: # True
-    data = s.recv(config['max req len'])
+    data = s.recv(config['max_req_len'])
     if (len(data)>0):
         conn.send(data) # send to browser/client
     else:
